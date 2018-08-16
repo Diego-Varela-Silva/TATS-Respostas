adf744db216281e4e3e9bd3be538430a9ecd8570
Diego Varela <diegovarela.paiva@hotmail.com>
2018-08-16 20:47:07 -0300

    Cria teste para desenvolvedor com salario maior que 3000

---
+++ Atividade04/test/CalculadoraSalarioLuquidoTest.java
@@ -0,0 +1,15 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class CalculadoraSalarioLuquidoTest {
+    
+    @Test
+    public void desenvolvedorSalarioMaior3000() {
+        Funcionario funcionario = new Funcionario();
+        funcionario.setCargo("DESENVOLVEDOR");
+        funcionario.setSalarioBase(3500F);
+        CalculadoraSalarioLiquido calculadoraSalarioLiquido =  new CalculadoraSalarioLiquido();
+        Assert.assertEquals(2800F, calculadoraSalarioLiquido.calcular(funcionario), 0.1);
+    }
+    
+}
